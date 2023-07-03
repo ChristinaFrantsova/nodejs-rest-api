54bec55fa38eabdcb822f717526e23fb73bfd389
ChristinaFrantsova <mob0964442737@gmail.com>
2023-07-03 17:40:56 +0300

    split the routs with the controllers

---
+++ controllers/contacts.js
@@ -0,0 +1,89 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../models/contacts");
+
+const addSchema = require("../middlewares/validation");
+
+const getAll = async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const getById = async (req, res, next) => {
+  console.log(req.params);
+  try {
+    const { contactId } = req.params;
+    const contactById = await getContactById(contactId);
+
+    if (!contactById) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(contactById);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    // console.log(error.details);
+    const labelName = error.details[0].context.label;
+    if (error) {
+      return res
+        .status(400)
+        .json({ message: `Missing required ${labelName} field` });
+    }
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const remove = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const deletedContact = await removeContact(contactId);
+    if (deletedContact === null) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json({ message: "Contact deleted" });
+  } catch (error) {
+    res.status(404).json({ message: "Not found" });
+  }
+};
+
+const updateById = async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+    const { contactId } = req.params;
+    const updatedContact = await updateContact(contactId, req.body);
+
+    if (updatedContact === null) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    res.status(404).json({ message: "Not found" });
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  remove,
+  updateById,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,86 +1,21 @@
 const express = require("express");
 const router = express.Router();
-
 const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../../models/contacts");
+  getAll,
+  getById,
+  add,
+  remove,
+  updateById,
+} = require("../../controllers/contacts");
 
-const addSchema = require("../../middlewares/validation");
+router.get("/", getAll);
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.status(200).json(contacts);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
+router.get("/:contactId", getById);
 
-router.get("/:contactId", async (req, res, next) => {
-  console.log(req.params);
-  try {
-    const { contactId } = req.params;
-    const contactById = await getContactById(contactId);
+router.post("/", add);
 
-    if (!contactById) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    res.status(200).json(contactById);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
+router.delete("/:contactId", remove);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    // console.log(error.details);
-    const labelName = error.details[0].context.label;
-    if (error) {
-      return res
-        .status(400)
-        .json({ message: `Missing required ${labelName} field` });
-    }
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const deletedContact = await removeContact(contactId);
-    if (deletedContact === null) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    res.status(200).json({ message: "Contact deleted" });
-  } catch (error) {
-    res.status(404).json({ message: "Not found" });
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ message: "Missing fields" });
-    }
-    const { contactId } = req.params;
-    const updatedContact = await updateContact(contactId, req.body);
-
-    if (updatedContact === null) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    res.status(200).json(updatedContact);
-  } catch (error) {
-    res.status(404).json({ message: "Not found" });
-  }
-});
+router.put("/:contactId", updateById);
 
 module.exports = router;
