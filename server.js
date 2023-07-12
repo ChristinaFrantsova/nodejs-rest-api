05ad6e812fa7298fccbc41965ba60233a6eb8cf1
ChristinaFrantsova <mob0964442737@gmail.com>
2023-07-12 15:06:46 +0300

    add some changes
    1.Create an account on MongoDB Atlas.
    2.Install MongoDB Compass graphical editor desktop version and create a database
    3.Connected to MongoDB using Mongoose (add code to server.js file)

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+// mongoDB Atlas user Pas: grFbBT4IqBUxc4WF;
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Chris:grFbBT4IqBUxc4WF@cluster0.4so3xez.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
