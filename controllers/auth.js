db46dd64ea2280687a0134042386dd09dbe11e03
ChristinaFrantsova <mob0964442737@gmail.com>
2023-07-24 12:43:02 +0300

    add user register

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const { User } = require("../models/user");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const register = async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    email: newUser.email,
+    subscription: newUser.subscription,
+  });
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
+const { validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/user");
+
+const { register } = require("../../controllers/auth");
+
 const router = express.Router();
 
 // sing up
-router.post("/register");
+router.post("/register", validateBody(schemas.registerSchema), register);
 
 module.exports = router;
