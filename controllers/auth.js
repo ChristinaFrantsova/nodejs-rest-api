5c18dc67e7b12c717d681688bc6dc158e7e646e0
ChristinaFrantsova <mob0964442737@gmail.com>
2023-07-25 17:03:15 +0300

    fixed bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,8 +19,10 @@ const register = async (req, res, next) => {
   const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
-    email: newUser.email,
-    subscription: newUser.subscription,
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
   });
 };
 
@@ -40,7 +42,13 @@ const login = async (req, res, next) => {
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
   await User.findByIdAndUpdate(user._id, { token });
-  res.json({ token });
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
 };
 
 const getCurrent = async (req, res, next) => {
